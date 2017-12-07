d665aa28d41542f8a58453fa61cf3f1cfc22f2bc
M.Charattrakool <m.charattrakool@gmail.com>
2017-12-07 20:37:01 +0700

    use http

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type ResponsePayload struct {
+	Status string
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	var m ResponsePayload
+	m.Status = "OK"
+	b, _ := json.Marshal(m)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Fprintf(w, string(b))
+}
 
 func main() {
-	fmt.Println("test")
+	http.HandleFunc("/", homePage)
+	fmt.Println("listening and serving on PORT 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
