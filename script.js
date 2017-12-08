aa1b18764c253bed6668eb70fd140748b78fba08
M.Charattrakool <m.charattrakool@gmail.com>
2017-12-09 01:12:34 +0700

    can tell class and private/public

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ $("#formid").submit(function(event) {
 
   var $form = $(this),
     url = $form.attr('action');
+    option = $('#subnet').val().split("/");
 
+    var cls = "none"
+    var optcls = parseInt(option[1])
+    if (optcls >= 24) {
+      cls = "C"
+    } else if (optcls >= 16) {
+      cls = "B"
+    } else if (optcls >= 8) {
+      cls = "A"
+    }
+    var ipType =  /(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/.test($("#ip").val())?"Private":"Public"
   /* Send the data using post with element id name and name2*/
   var posting = $.post(url, {
     ip: $('#ip').val(),
-    subnet: $('#subnet').val()
+    subnet: option[0]
   });
+
 
   /* Alerts the results */
   posting.done(function(data) {
@@ -60,19 +72,25 @@ $("#formid").submit(function(event) {
     $('#result').append(" \
           <tr> \
             <td>IP Class</td> \
-            <td>" + "None" + "</td>\
+            <td>" + cls + "</td>\
+          </tr>");
+
+    $('#result').append(" \
+          <tr> \
+            <td>IP Type</td> \
+            <td>"+ipType +"</td>\
           </tr>");
 
     $('#result').append(" \
           <tr> \
             <td>CIDR Notation</td> \
-            <td>" +""+ "</td>\
+            <td>/"+ option[1] +"</td>\
           </tr>");
 
     $('#result').append(" \
           <tr> \
             <td>Short</td> \
-            <td>" + data.IP + "</td>\
+            <td>" + data.IP+ "/"+option[1] + "</td>\
           </tr>");
 
     $('#result').append(" \
